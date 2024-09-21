e3146fe34b0169784f15288fe1fb9ae51a14e47f
Pablo Isla Casares <itc.import.export@gmail.com>
2024-09-20 23:50:11 -0500

    Se modifica z-index de modal para que se superponga a mini modal con eventos

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -109,7 +109,7 @@ function ModalEvent({ isOpen, onClose, onSave, onDelete, event }) {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-gray-800 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center z-50">
+    <div className="fixed inset-0 bg-gray-800 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center z-[9999]">
       <div className="bg-white p-8 rounded-lg shadow-2xl w-full max-w-4xl max-h-[90vh] overflow-y-auto">
         <div className="flex justify-between items-center mb-6">
           <h2 className="text-3xl font-bold text-gray-900">
